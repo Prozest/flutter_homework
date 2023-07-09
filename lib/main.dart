dd7d8fdcfc9666b04556a7cc67d7603631356db6
Prozest <76399209+Prozest@users.noreply.github.com>
2023-07-09 15:36:49 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_homework/pages/home/home_page.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
