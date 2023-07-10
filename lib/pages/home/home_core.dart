bea4eaef19c208a84a8b0a098820c0b32a1fbb61
Prozest <76399209+Prozest@users.noreply.github.com>
2023-07-10 15:28:46 +0500

    Logout

--- lib/pages/home/home_core.dart
+++ lib/pages/home/home_core.dart
@@ -1,6 +1,10 @@
-class HomeCore{
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
 
-  static String title(){
-    return "Hello";
+class HomeCore extends GetxController {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void logout(){
+    FirebaseAuth.instance.signOut();
   }
 }
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_homework/pages/home/home_core.dart';
+import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+ const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+ @override
+ Widget build(BuildContext context) {
+   var model = Get.put<HomeCore>(HomeCore());
 
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(HomeCore.title()),
+   return Scaffold(
+    body: Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: model.user.email!.text.size(20).center.make()
+          ),
+          const SizedBox(height: 10,),
+          ElevatedButton(
+            onPressed: (){
+              model.logout();
+            },
+            child: "Logout".text.make()
+          )
+        ]
       ),
-    );
+    ),
+   );
   }
 }
